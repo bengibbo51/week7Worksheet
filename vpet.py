0102ec0a7a56561fee7734d8d6a3f828158f4393
bengibbo51 <wkft0860@leeds.ac.uk>
2025-11-22 15:58:30 +0000

    virtual pet 2

--- vpet.py
+++ vpet.py
@@ -31,27 +31,34 @@ class VirtualPet:
 
     def play(self):
         if self.energy < 2:
-            print(f"{self.name} is too tired to play!")
+            return f"{self.name} is too tired to play!"
         else:
-            self.energy - 2
-            self.hunger + 3
+            self.energy = self.energy - 2
+            self.hunger = self.hunger + 2
 
     def feed(self):
+        self.hunger = self.hunger - 3
         if self.hunger < 0:
-            print(f"{self.name} is overfed!")
-            self.hunger = 0
-        else:
-            self.hunger - 3
+            self.hunger = self.hunger = 0
+            return f"{self.name} is overfed!"
 
     def sleep(self):
-        self.energy + 10
+        self.energy = self.energy + 10
 
     def __str__(self):
-        
+        return f"{self.name} has {self.energy} energy points and hunger level {self.hunger}"
+    
+    def __eq__(self, other):
+        if not isinstance(other, VirtualPet):
+            return False
+        return (self.name == other.name and 
+                self.energy == other.energy and 
+                self.hunger == other.hunger)
+
+#pet = VirtualPet("Timmy",4,3)
+#print(pet)
 
 
-
-''' Tests
 Pet = VirtualPet("Timmy",4,3)
 print(Pet)
 Pet.play()
@@ -60,4 +67,3 @@ Pet.feed()
 print(Pet)
 Pet.sleep()
 print(Pet)
-'''
